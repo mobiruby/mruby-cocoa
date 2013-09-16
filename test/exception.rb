baa8c639f77d0b33fe6351046e20fd02535fd2ef
Yuichiro MASUI <masui@masuidrive.jp>
2013-09-17 01:01:19 +0900

    Fixed wrong test. tested on latest mruby.

--- test/exception.rb
+++ test/exception.rb
@@ -1,6 +1,6 @@
 mobiruby_test "Cocoa::catch exception" do
   class Cocoa::MobiCocoaExceptionTest < Cocoa::NSObject
-    define_class CFunc::Void, :raise_exception do |i|
+    define_class CFunc::Void, :raise_exception do
       Cocoa::NSException._raise Cocoa::NSString._stringWithUTF8String("TestException"), :format, Cocoa::NSString._stringWithUTF8String("Test")
     end
   end
